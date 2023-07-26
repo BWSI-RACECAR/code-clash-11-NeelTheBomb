e54f711fa4dccf03eb7b5db7ca91682b4f3c8ba7
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2023-07-26 09:24:58 -0400

    Update increasingsubsequence.py

--- increasingsubsequence.py
+++ increasingsubsequence.py
@@ -6,22 +6,22 @@ class Solution:
             print(arr)
             #type arr: list of int
             #return type: int
-            arr = arr.sort()
-            # for i in arr:
-            #     if i > prev:
-            #         count = count+1
-            #     prev = i
-            for i in range(len(arr)):
-                if arr[i] > prev:
-                    count = count + 1
-                    if count > greatest:
-                        greatest = count
-                else:
-                     count = 0
-                prev = arr[i]
-            # #if greatest == 0:
-            #    # greatest = 1
-            print(greatest)
+            # arr = arr.sort()
+            for i in arr:
+                if i > prev:
+                    count = count+1
+                prev = i
+            # for i in range(len(arr)):
+            #     if arr[i] > prev:
+            #         count = count + 1
+            #         if count > greatest:
+            #             greatest = count
+            #     else:
+            #          count = 0
+            #     prev = arr[i]
+            # # #if greatest == 0:
+            # #    # greatest = 1
+            # print(greatest)
             return count
                  
             
